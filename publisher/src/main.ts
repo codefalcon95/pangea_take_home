1acd1b99f44c3688554135fee3dc8f46d4965733
Sanni Moshood Abiola <sanniabiolamoshood95@gmail.com>
2021-12-22 04:10:16 +0100

    feat(publisher): make port dynamic

--- publisher/src/main.ts
+++ publisher/src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.APP_PORT || 8000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  await app.listen(PORT, () => console.log(`Server up on port ${PORT} !!!`));
 }
 bootstrap();
